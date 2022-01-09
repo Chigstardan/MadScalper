ce00edd43dddbfe6818fd4496c4d238139da7855
Chigozie Daniel <91606695+Chigstardan@users.noreply.github.com>
2022-01-09 23:40:50 +0100

    Add files via upload

--- crypto_bot.py
+++ crypto_bot.py
@@ -110,7 +110,7 @@ def strategy(pair, qty):
 					if len(dfx) > 0:
 						break
 				break
-			if df.Close.iloc[-2] < df.ema3.iloc[-2]:
+			if (buyprice - (df.ATR.iloc[-1] * 0.5)) > df.Close.iloc[-1]:
 				order = client.futures_create_order(symbol=pair, 
 		                            side='SELL',
 		                            type='MARKET',
@@ -161,7 +161,7 @@ def strategy(pair, qty):
 					if len(dfx) > 0:
 						break
 				break
-			if df.Close.iloc[-2] > df.ema3.iloc[-2]:	
+			if (sellprice + (df.ATR.iloc[-1] * 0.5)) > df.Close.iloc[-1]:	
 				order = client.futures_create_order(symbol=pair, 
 		                            			side='BUY',
 		                           			 type='MARKET',
