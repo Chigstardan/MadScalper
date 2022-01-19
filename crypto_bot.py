4fa7538a1c01fbaffad69e705a60208879bfb0e5
Chigozie Daniel <91606695+Chigstardan@users.noreply.github.com>
2022-01-19 09:46:38 +0100

    Add files via upload

--- crypto_bot.py
+++ crypto_bot.py
@@ -21,10 +21,7 @@ def applytechnicals(df, dx):
 	df['ema3'] = ta.trend.ema_indicator(df.Close, window=3)
 	df['ema6'] = ta.trend.ema_indicator(df.Close, window=6)
 	df['ema9'] = ta.trend.ema_indicator(df.Close, window=9)
-	df['ema2'] = ta.trend.ema_indicator(df.Close, window=2)
-	df['rsi'] = ta.momentum.rsi(df.Close, window=5)
-	df['ADX'] = ta.trend.adx(df.High, df.Low, df.Close, window=3)
-	df['ATR'] = ta.volatility.average_true_range(df.High, df.Low, df.Close, window=14)
+	df['ATR'] = ta.volatility.average_true_range(df.High, df.Low, df.Close, window=10)
 	df['macd'] = ta.trend.macd_diff(df.Close, window_slow=6, window_fast=3, window_sign=3)
 	df.dropna(inplace=True)
 	dx['ema3'] = ta.trend.ema_indicator(dx.Close, window=3)
@@ -43,14 +40,14 @@ class Signals:
 		                       & (self.df.ema6[-1] > self.df.ema9[-1])
 		                       & (self.df.ema3[-1] > self.df.ema6[-1])
 		                       & (self.df.Open.iloc[-2] < self.df.Close.iloc[-2])
-		                       & (self.df.Low.iloc[-2] < self.df.ema6[-2] )
-		                       & (self.df.macd > 0), 1, 0)
+		                       & (self.df.Low[-2] < self.df.ema6[-2] )
+		                       & (self.df.macd[-2]] > 0), 1, 0)
 		self.df['Sell'] = np.where((self.df.ema3[-1] < self.df.ema9[-1])
 		                          & (self.df.ema6[-1] < self.df.ema9[-1])
 		                          & (self.df.ema3[-1] < self.df.ema6[-1])
 	                          	& (self.df.Open.iloc[-2] > self.df.Close.iloc[-2])
-	                          	& (self.df.High.iloc[-2] > self.df.ema6[-2])
-	                          	& (self.df.macd < 0), 1, 0)
+	                          	& (self.df.High[-2] > self.df.ema6[-2])
+	                          	& (self.df.macd[-2] < 0), 1, 0)
 		self.dx['HBuy'] = np.where((self.dx.ema3 > self.dx.ema9)
 		                       & (self.dx.ema6 > self.dx.ema9)
 		                       & (self.dx.ema3 > self.dx.ema6)
@@ -92,6 +89,23 @@ def strategy(pair, qty):
 			inst = Signals(df, dx)
 			inst.decide()
 			print(f'Current Close is '+str(df.Close.iloc[-1]))
+			if (buyprice + (df.ATR.iloc[-1] * 0.7)) < df.Close.iloc[-1]:
+				order = client.futures_create_order(symbol=pair, 
+		                            side='SELL',
+		                            type='MARKET',
+		                            quantity=qty)
+				print(order)
+				while True:
+					time.sleep(0.9)
+					df = GetMinuteData('ETHUSDT', '5m', '600')
+					dx = GetMinuteData('ETHUSDT', '5m', '600')
+					applytechnicals(df, dx)
+					inst = Signals(df, dx)
+					inst.decide()
+					df = df.loc[df.index > pd.to_datetime(order['updateTime'], unit='ms')]
+					if len(dfx) > 0:
+						break
+				break
 			if df.macd.iloc[-1] < 0:
 				order = client.futures_create_order(symbol=pair, 
 		                            side='SELL',
@@ -117,13 +131,30 @@ def strategy(pair, qty):
 		                            quantity=qty)
 		print(order)
 		while True:
-			time.sleep(0.9)
+			time.sleep(1)
 			df = GetMinuteData('ETHUSDT', '5m', '600')
 			dx = GetMinuteData('ETHUSDT', '5m', '600')
 			applytechnicals(df, dx)
 			inst = Signals(df, dx)
 			inst.decide()
 			print(f'Current Close is '+str(df.Close.iloc[-1]))
+			if (buyprice - (df.ATR.iloc[-1] * 0.7)) > df.Close.iloc[-1]:
+				order = client.futures_create_order(symbol=pair, 
+		                            side='BUY',
+		                            type='MARKET',
+		                            quantity=qty)
+				print(order)
+				while True:
+					time.sleep(0.9)
+					df = GetMinuteData('ETHUSDT', '5m', '600')
+					dx = GetMinuteData('ETHUSDT', '5m', '600')
+					applytechnicals(df, dx)
+					inst = Signals(df, dx)
+					inst.decide()
+					df = df.loc[df.index > pd.to_datetime(order['updateTime'], unit='ms')]
+					if len(dfx) > 0:
+						break
+				break
 			if df.macd.iloc[-1] > 0:
 				order = client.futures_create_order(symbol=pair, 
 		                            side='BUY',
